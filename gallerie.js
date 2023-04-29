558e34a3caa971d73d338a562d239843961b31d4
Charlotte Lft <charlotte.lft@hotmail.be>
2023-04-29 10:35:55 +0200

    début creation anmation gallerie en JS

---
+++ gallerie.js
@@ -0,0 +1,89 @@
+// selectors images - gallerie
+let img1 = document.querySelector('img-1');
+let img2 = document.querySelector('img-2');
+let img3 = document.querySelector('img-3');
+let img4 = document.querySelector('img-4');
+let img5 = document.querySelector('img-5');
+let img6 = document.querySelector('img-6');
+let img7 = document.querySelector('img-7');
+// selectors buttons
+let btn1 = document.querySelector('caption-btn-1');
+let btn2 = document.querySelector('caption-btn-2');
+let btn3 = document.querySelector('caption-btn-3');
+let btn4 = document.querySelector('caption-btn-4');
+let btn5 = document.querySelector('caption-btn-5');
+let btn6 = document.querySelector('caption-btn-6');
+let btn7 = document.querySelector('caption-btn-7');
+
+
+
+btn1.addEventListener('click', () => {
+  img1.src = 'images/bebe/baby-1.jpg';
+  img2.src = 'images/bebe/baby-2.jpg';
+  img3.src = 'images/bebe/baby-3.jpg';
+  img4.src = 'images/bebe/baby-4.jpg';
+  img5.src = 'images/bebe/baby-5.jpg';
+  img6.src = 'images/bebe/baby-6.jpg';
+  img7.src = 'images/bebe/baby-7.jpg';
+});
+
+btn2.addEventListener('click', ()=> {
+  document.getElementById('img-1').src ='images/mariage/wedding-1.jpg';
+  img2.src = 'images/mariage/wedding-2.jpg';
+  img3.src = 'images/mariage/wedding-3.jpg';
+  img4.src = 'images/mariage/wedding-4.jpg';
+  img5.src = 'images/mariage/wedding-5.jpg';
+  img6.src = 'images/mariage/wedding-6.jpg';
+  img7.src = 'images/mariage/wedding-7.jpg';
+});
+
+btn3.addEventListener('click', () => {
+  img1.src = 'images/bebe/baby-1.jpg';
+  img2.src = 'images/bebe/baby-2.jpg';
+  img3.src = 'images/bebe/baby-3.jpg';
+  img4.src = 'images/bebe/baby-4.jpg';
+  img5.src = 'images/bebe/baby-5.jpg';
+  img6.src = 'images/bebe/baby-6.jpg';
+  img7.src = 'images/bebe/baby-7.jpg';
+});
+
+btn4.addEventListener('click', ()=> {
+  img1.src = 'images/bebe/baby-1.jpg';
+  img2.src = 'images/bebe/baby-2.jpg';
+  img3.src = 'images/bebe/baby-3.jpg';
+  img4.src = 'images/bebe/baby-4.jpg';
+  img5.src = 'images/bebe/baby-5.jpg';
+  img6.src = 'images/bebe/baby-6.jpg';
+  img7.src = 'images/bebe/baby-7.jpg';
+});
+
+btn5.addEventListener('click', ()=> {
+  img1.src = 'images/bebe/baby-1.jpg';
+  img2.src = 'images/bebe/baby-2.jpg';
+  img3.src = 'images/bebe/baby-3.jpg';
+  img4.src = 'images/bebe/baby-4.jpg';
+  img5.src = 'images/bebe/baby-5.jpg';
+  img6.src = 'images/bebe/baby-6.jpg';
+  img7.src = 'images/bebe/baby-7.jpg';
+});
+
+btn6.addEventListener('click', ()=> {
+  img1.src = 'images/bebe/baby-1.jpg';
+  img2.src = 'images/bebe/baby-2.jpg';
+  img3.src = 'images/bebe/baby-3.jpg';
+  img4.src = 'images/bebe/baby-4.jpg';
+  img5.src = 'images/bebe/baby-5.jpg';
+  img6.src = 'images/bebe/baby-6.jpg';
+  img7.src = 'images/bebe/baby-7.jpg';
+});
+
+btn7.addEventListener('click', ()=> {
+  img1.src = 'images/bebe/baby-1.jpg';
+  img2.src = 'images/bebe/baby-2.jpg';
+  img3.src = 'images/bebe/baby-3.jpg';
+  img4.src = 'images/bebe/baby-4.jpg';
+  img5.src = 'images/bebe/baby-5.jpg';
+  img6.src = 'images/bebe/baby-6.jpg';
+  img7.src = 'images/bebe/baby-7.jpg';
+}); 
+
